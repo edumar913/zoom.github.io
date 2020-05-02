4858b9ef7cab40dd148d52f97e54d781132bc3d0
edumar913 <50591821+edumar913@users.noreply.github.com>
2020-05-01 21:44:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,79 @@
+var heading;
+heading = document.getElementById('colorValue');
+heading.innerHTML = 'Hello world!';
+function setButtonColour(button, red, green, blue){
+    button.setAttribute('style',
+                        'background-color: rgb('+ red +','+ green +','+ blue +');'
+                       );
+}
+var buttons = document.getElementsByClassName('colourButton');
+buttons[0]
+setButtonColour(buttons[0], 0, 0, 255);
+function makeColourValue(){
+    return Math.round(Math.random()*255);
+}
+var red = makeColourValue();
+var green = makeColourValue();
+var blue = makeColourValue();
 
+setButtonColour(buttons[0], red, green, blue);
+for (var i = 0; i < buttons.length; i++) {
+
+  var red = makeColourValue();
+  var green = makeColourValue();
+  var blue = makeColourValue();
+
+  setButtonColour(buttons[i], red, green, blue);
+
+}
+var answerButton = Math.round(Math.random() * (buttons.length - 1));
+for (var i = 0; i < buttons.length; i++) {
+
+  var red = makeColourValue();
+  var green = makeColourValue();
+  var blue = makeColourValue();
+
+  setButtonColour(buttons[i], red, green, blue);
+
+  if (i === answerButton) {
+    heading.innerHTML = `(${red}, ${green}, ${blue})`;
+  }
+}
+var answerMessage = document.getElementById('answer');
+buttons[i].addEventListener('click', function(){
+        if (this === buttons[answerButton]) {
+            answerMessage.innerHTML = "Correct!";
+        } else {
+            answerMessage.innerHTML = "Wrong answer! Guess again!";
+        }
+    });
+function startGame() {
+
+  var answerButton = Math.round(Math.random() * (buttons.length - 1));
+
+  for (var i = 0; i < buttons.length; i++) {
+
+    var red = makeColourValue();
+    var green = makeColourValue();
+    var blue = makeColourValue();
+
+    setButtonColour(buttons[i], red, green, blue);
+
+    if (i === answerButton) {
+      heading.innerHTML = `(${red}, ${green}, ${blue})`;
+    }
+
+    buttons[i].addEventListener('click', function(){
+        if (this === buttons[answerButton]) {
+            answerMessage.innerHTML = "Correct!";
+        } else {
+            answerMessage.innerHTML = "Wrong answer! Guess again!";
+        }
+    });
+
+  }
+
+}
+startGame();
+answerMessage.innerHTML = "";
+document.getElementById('resetButton').addEventListener('click', startGame);
