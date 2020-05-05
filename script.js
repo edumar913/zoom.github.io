ad16e995b2de87084f30c2bfced58c2c1d4e2ad0
edumar913 <50591821+edumar913@users.noreply.github.com>
2020-05-05 08:27:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,16 @@
-var heading;
-heading = document.getElementById('colorValue');
-heading.innerHTML = 'Hello world!';
-function setButtonColour(button, red, green, blue){
-    button.setAttribute('style',
-                        'background-color: rgb('+ red +','+ green +','+ blue +');'
-                       );
-}
 var buttons = document.getElementsByClassName('colourButton');
-buttons[0]
 setButtonColour(buttons[0], 0, 0, 255);
 function makeColourValue(){
     return Math.round(Math.random()*255);
 }
-var red = makeColourValue();
-var green = makeColourValue();
-var blue = makeColourValue();
-
-setButtonColour(buttons[0], red, green, blue);
-for (var i = 0; i < buttons.length; i++) {
-
-  var red = makeColourValue();
-  var green = makeColourValue();
-  var blue = makeColourValue();
-
-  setButtonColour(buttons[i], red, green, blue);
-
+var heading;
+heading = document.getElementById('colourValue');
+heading.innerHTML = 'Hello world!';
+function setButtonColour(button, red, green, blue){
+    button.setAttribute('style','background-color: rgb('+ red +','+ green +','+ blue +');');
 }
-var answerButton = Math.round(Math.random() * (buttons.length - 1));
-for (var i = 0; i < buttons.length; i++) {
-
-  var red = makeColourValue();
-  var green = makeColourValue();
-  var blue = makeColourValue();
-
-  setButtonColour(buttons[i], red, green, blue);
-
-  if (i === answerButton) {
-    heading.innerHTML = `(${red}, ${green}, ${blue})`;
-  }
-}
-var answerMessage = document.getElementById('answer');
-buttons[i].addEventListener('click', function(){
-        if (this === buttons[answerButton]) {
-            answerMessage.innerHTML = "Correct!";
-        } else {
-            answerMessage.innerHTML = "Wrong answer! Guess again!";
-        }
-    });
 function startGame() {
-
+  answerMessage.innerHTML = "";
   var answerButton = Math.round(Math.random() * (buttons.length - 1));
 
   for (var i = 0; i < buttons.length; i++) {
@@ -74,6 +36,6 @@ function startGame() {
   }
 
 }
+var answerMessage = document.getElementById('answer');
 startGame();
-answerMessage.innerHTML = "";
 document.getElementById('resetButton').addEventListener('click', startGame);
